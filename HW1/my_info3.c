ff2428d5868b74593ad8d8c81de133d5094ee19a
a43414786 <72087693+a43414786@users.noreply.github.com>
2021-10-30 15:44:00 +0800

    Complete show CPU information

--- HW1/my_info3.c
+++ HW1/my_info3.c
@@ -85,7 +85,7 @@ static const struct file_operations my_file_ops =
 
 };
 
-static int __init init(void)
+static int __init my_init(void)
 
 {
 
@@ -97,7 +97,7 @@ static int __init init(void)
 
 }
 
-static void __exit exit(void)
+static void __exit my_exit(void)
 
 {
 
@@ -105,8 +105,8 @@ static void __exit exit(void)
 
 }
 
-module_init(init);
+module_init(my_init);
 
-module_exit(exit);
+module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
