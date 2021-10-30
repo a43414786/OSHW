1490248daf322cb02a751b9ba953420d518a3cfd
a43414786 <72087693+a43414786@users.noreply.github.com>
2021-10-30 15:25:44 +0800

    Complete show Memory information

--- HW1/my_info4.c
+++ HW1/my_info4.c
@@ -47,7 +47,6 @@ static void *start(struct seq_file *m, loff_t *pos)
     return NULL;
 }
 
-
 static void *next(struct seq_file *m, void *v, loff_t *pos)
 {
     (*pos)++;
@@ -104,7 +103,6 @@ const struct seq_operations info_op =
     .show	= show,
 };
 
-
 static int my_open(struct inode *inode, struct file *file)
 
 {
@@ -112,8 +110,6 @@ static int my_open(struct inode *inode, struct file *file)
     return seq_open(file, &info_op);
 
 };
-
-
 
 static const struct file_operations my_file_ops =
 {
@@ -124,7 +120,7 @@ static const struct file_operations my_file_ops =
 
 };
 
-static int __init procfs_init(void)
+static int __init init(void)
 
 {
 
@@ -136,24 +132,16 @@ static int __init procfs_init(void)
 
 }
 
-
-
-static void __exit procfs_exit(void)
+static void __exit exit(void)
 
 {
 
     remove_proc_entry(PROC_NAME, NULL);
 
-    pr_debug("/proc/%s removed\n", PROC_NAME);
-
 }
 
+module_init(init);
 
-
-module_init(procfs_init);
-
-module_exit(procfs_exit);
-
-
+module_exit(exit);
 
 MODULE_LICENSE("GPL");
