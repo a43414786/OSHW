5c4d78a022fa78eec54d17d6845b336b31f305e1
a43414786 <72087693+a43414786@users.noreply.github.com>
2021-10-30 15:46:55 +0800

    Complete show uptime information

--- HW1/my_info.c
+++ HW1/my_info.c
@@ -152,7 +152,7 @@ static const struct file_operations my_file_ops =
 
 };
 
-static int __init init(void)
+static int __init my_init(void)
 
 {
 
@@ -164,7 +164,7 @@ static int __init init(void)
 
 }
 
-static void __exit exit(void)
+static void __exit my_exit(void)
 
 {
 
@@ -172,8 +172,8 @@ static void __exit exit(void)
 
 }
 
-module_init(init);
+module_init(my_init);
 
-module_exit(exit);
+module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
