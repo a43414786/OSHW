1c32b4be308d93623238d758a9cedecb1d5c644e
a43414786 <a43414786@gmail.com>
2021-10-30 12:13:34 +0800

    k

--- HW1/app.c
+++ HW1/app.c
@@ -5,122 +5,150 @@
 
 typedef struct node* nodeptr;
 
-struct node{
-	char c;
-	nodeptr next;
+struct node
+{
+    char c;
+    nodeptr next;
 };
-nodeptr createnode(char c){
-	nodeptr temp = malloc(sizeof(nodeptr));
-	temp->c = c;
-	temp->next = NULL;
-	return temp;	
+nodeptr createnode(char c)
+{
+    nodeptr temp = malloc(sizeof(nodeptr));
+    temp->c = c;
+    temp->next = NULL;
+    return temp;
 }
-void addnode(nodeptr *root,char c){
-	nodeptr temp = *root;
-	if(temp != NULL){
-		while(temp->next != NULL){
-			temp = temp->next;
-		}
-		temp->next = createnode(c);
-	}else{
-		*root = createnode(c);
-	}
-}
-
-void print_info(nodeptr root){
-
-	while(root){
-	
-		printf("%c",root->c);
-		root = root->next;
-	
-	}
-
+void addnode(nodeptr *root,char c)
+{
+    nodeptr temp = *root;
+    if(temp != NULL)
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = createnode(c);
+    }
+    else
+    {
+        *root = createnode(c);
+    }
 }
 
-void print_device_info(nodeptr all,nodeptr *ptrs,char input){
-	switch(input){
-		case 'v':
-			printf("Version: ");
-			print_info(ptrs[0]);
-			break;
-		case 'c':
-			printf("Cpu information:\n");
-			print_info(ptrs[1]);
-			break;
-		case 'm':
-			printf("Memory information:\n");
-			print_info(ptrs[2]);
-			break;
-		case 't':
-			printf("Time information:\n");
-			print_info(ptrs[3]);
-			break;
-		case 'a':
-			print_info(all);
-			break;
-		default:
-			break;
-	}
+void print_info(nodeptr root)
+{
+
+    while(root)
+    {
+
+        printf("%c",root->c);
+        root = root->next;
+
+    }
 
 }
-int main(){
-	char word,input = ' ';
-	short flag1 = 0,flag2 = 0;
-	nodeptr all = NULL,ptrs[4] = {NULL,NULL,NULL,NULL};
-	int counter = -1;
-	FILE* myfile = fopen(MY_INFO,"rb");
 
-	while((word = fgetc(myfile)) != EOF ){
-		addnode(&all,word);
-		if(word == '='){
-			flag1 = 1;
-			flag2 = 0;
-		}
-		if(flag2){
-			addnode(&ptrs[counter],word);
-		}
-		if(flag1){
-			
-			if(word == '\n'){
-				
-				if(counter >= 0){
-					
-					for(int i = 0 ; i < 65 ; i++){
-					
-						addnode(&ptrs[counter],'-');
-					
-					}
-				
-					addnode(&ptrs[counter],'\n');
-					addnode(&ptrs[counter],'\n');
-			
-				}
-				counter++;
-				flag1 = 0;
-				flag2 = 1;
-			}
-		}
-		
-	}
-	addnode(&all,'\n');
-	addnode(&ptrs[counter],'\n');
-	for(int i = 0 ; i < 65 ; i++){
-		addnode(&all,'-');
-		addnode(&ptrs[counter],'-');
-		
-	}
-	addnode(&all,'\n');
-	addnode(&all,'\n');	
-	addnode(&ptrs[counter],'\n');
-	addnode(&ptrs[counter],'\n');
+void print_device_info(nodeptr all,nodeptr *ptrs,char input)
+{
+    switch(input)
+    {
+    case 'v':
+        printf("Version: ");
+        print_info(ptrs[0]);
+        break;
+    case 'c':
+        printf("Cpu information:\n");
+        print_info(ptrs[1]);
+        break;
+    case 'm':
+        printf("Memory information:\n");
+        print_info(ptrs[2]);
+        break;
+    case 't':
+        printf("Time information:\n");
+        print_info(ptrs[3]);
+        break;
+    case 'a':
+        print_info(all);
+        break;
+    default:
+        break;
+    }
 
-	for(;input != 'e';){
-		printf("Which information do you want?\nVersion(v),CPU(c),Memory(m),Time(t),All(a),Exit(e)?\n");
-		scanf("\n%c",&input);
+}
+int main()
+{
+    char word,input = ' ';
+    short flag1 = 0,flag2 = 0;
+    nodeptr all = NULL,ptrs[4] = {NULL,NULL,NULL,NULL};
+    int counter = -1;
+    FILE* myfile = fopen(MY_INFO,"rb");
 
-		print_device_info(all,ptrs,input);
-	}
-	return 0;
+    while((word = fgetc(myfile)) != EOF )
+    {
+        addnode(&all,word);
+        if(word == '=')
+        {
+            flag1 = 1;
+            flag2 = 0;
+        }
+        if(flag2)
+        {
+            addnode(&ptrs[counter],word);
+        }
+        if(flag1)
+        {
+
+            if(word == '\n')
+            {
+
+                if(counter >= 0)
+                {
+
+                    for(int i = 0 ; i < 65 ; i++)
+                    {
+
+                        addnode(&ptrs[counter],'-');
+
+                    }
+
+                    addnode(&ptrs[counter],'\n');
+                    addnode(&ptrs[counter],'\n');
+
+                }
+                counter++;
+                flag1 = 0;
+                flag2 = 1;
+            }
+        }
+
+    }
+    addnode(&all,'\n');
+    addnode(&ptrs[counter],'\n');
+    for(int i = 0 ; i < 65 ; i++)
+    {
+        addnode(&all,'-');
+        addnode(&ptrs[counter],'-');
+
+    }
+    addnode(&all,'\n');
+    addnode(&all,'\n');
+    addnode(&ptrs[counter],'\n');
+    addnode(&ptrs[counter],'\n');
+
+    while(input != 'e')
+    {
+        printf("Which information do you want?\nVersion(v),CPU(c),Memory(m),Time(t),All(a),Exit(e)?\n");
+        scanf("\n%c",&input);
+
+        print_device_info(all,ptrs,input);
+    }
+
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        free(ptrs[i]);
+    }
+    free(all);
+
+    return 0;
 }
 
--- HW1/my_info.c
+++ HW1/my_info.c
@@ -1,4 +1,4 @@
-#include <linux/module.h> 
+#include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/utsname.h>
@@ -20,180 +20,182 @@
 #include <linux/time.h>
 #include <linux/kernel_stat.h>
 
-#define PROC_NAME "my_info" 
+#define PROC_NAME "my_info"
 
 static int show_my_info(struct seq_file *m, void *v)
 {
-        
-
-
-        struct cpuinfo_x86 *c = v;
-
-        struct sysinfo i;
-	unsigned long pages[NR_LRU_LISTS];
-	int lru;
-
-        struct timespec uptime;
-	struct timespec idle;
-	u64 nsec;
-	u32 rem;
-	int j;
-
-	/*version information*/
-        seq_printf(m, "=============Version=============\n");
-	seq_printf(m, "Linux version %s\n",utsname()->release);
-	/*cpu information*/
-
-        seq_printf(m, "\n=============CPU================\n");
-
-	seq_printf(m, "processor\t: %u\n"
-		   "model name\t: %s\n"
-		   "physical id\t: %d\n"
-		   "core id\t\t: %d\n"
-		   "cpu cores\t: %d\n"
-		   "cache size\t: %u KB\n"
-		   "clflush size\t: %u\n"
-		   "cache_alignment\t: %d\n"
-		   "address sizes\t: %u bits physical, %u bits virtual\n",
-		   c->cpu_index,
-		   c->x86_model_id[0] ? c->x86_model_id : "unknown",
-		   c->phys_proc_id,
-		   c->cpu_core_id,
-		   c->booted_cores,
-		   c->x86_cache_size,
-		   c->x86_clflush_size,
-		   c->x86_cache_alignment,
-		   c->x86_phys_bits, c->x86_virt_bits
-		   );
-        /*mem informatiom*/
-
-        seq_printf(m, "\n============Memory==============\n");
-
-	si_meminfo(&i);
-
-        for (lru = LRU_BASE; lru < NR_LRU_LISTS; lru++)
-		pages[lru] = global_node_page_state(NR_LRU_BASE + lru);
-
-	seq_printf(m, "MemTotal\t: %lu kB\n", i.totalram << (PAGE_SHIFT - 10));
-	seq_printf(m, "MemFree\t\t: %lu kB\n", i.freeram << (PAGE_SHIFT - 10));
-	
-	seq_printf(m, "Buffers\t\t: %lu kB\n", i.bufferram << (PAGE_SHIFT - 10));
-	seq_printf(m, "Active\t\t: %lu kB\n", (pages[LRU_ACTIVE_ANON] +
-					   pages[LRU_ACTIVE_FILE]) << (PAGE_SHIFT - 10));
-	seq_printf(m, "Inactive\t: %lu kB\n", (pages[LRU_INACTIVE_ANON] +
-					   pages[LRU_INACTIVE_FILE]) << (PAGE_SHIFT - 10));
-
-
-	seq_printf(m, "Dirty\t\t: %lu kB\n",
-		    global_node_page_state(NR_FILE_DIRTY) << (PAGE_SHIFT - 10));
-	seq_printf(m, "Writeback\t: %lu kB\n",
-		    global_node_page_state(NR_WRITEBACK) << (PAGE_SHIFT - 10));
-
-	seq_printf(m, "Shmem\t\t: %lu kB\n", i.sharedram << (PAGE_SHIFT - 10));
-	
-	seq_printf(m, "KernelStack\t: %lu kB\n",
-		   global_zone_page_state(NR_KERNEL_STACK_KB));
-	seq_printf(m, "PageTables\t: %lu kB\n",
-		    global_zone_page_state(NR_PAGETABLE) << (PAGE_SHIFT - 10));
 
 
 
+    struct cpuinfo_x86 *c = v;
+
+    struct sysinfo i;
+    unsigned long pages[NR_LRU_LISTS];
+    int lru;
+
+    struct timespec uptime;
+    struct timespec idle;
+    u64 nsec;
+    u32 rem;
+    int j;
+
+    /*version information*/
+    seq_printf(m, "=============Version=============\n");
+    seq_printf(m, "Linux version %s\n",utsname()->release);
+    /*cpu information*/
+
+    seq_printf(m, "\n=============CPU================\n");
+
+    seq_printf(m, "processor\t: %u\n"
+               "model name\t: %s\n"
+               "physical id\t: %d\n"
+               "core id\t\t: %d\n"
+               "cpu cores\t: %d\n"
+               "cache size\t: %u KB\n"
+               "clflush size\t: %u\n"
+               "cache_alignment\t: %d\n"
+               "address sizes\t: %u bits physical, %u bits virtual\n",
+               c->cpu_index,
+               c->x86_model_id[0] ? c->x86_model_id : "unknown",
+               c->phys_proc_id,
+               c->cpu_core_id,
+               c->booted_cores,
+               c->x86_cache_size,
+               c->x86_clflush_size,
+               c->x86_cache_alignment,
+               c->x86_phys_bits, c->x86_virt_bits
+              );
+    /*mem informatiom*/
+
+    seq_printf(m, "\n============Memory==============\n");
+
+    si_meminfo(&i);
+
+    for (lru = LRU_BASE; lru < NR_LRU_LISTS; lru++)
+        pages[lru] = global_node_page_state(NR_LRU_BASE + lru);
+
+    seq_printf(m, "MemTotal\t: %lu kB\n", i.totalram << (PAGE_SHIFT - 10));
+    seq_printf(m, "MemFree\t\t: %lu kB\n", i.freeram << (PAGE_SHIFT - 10));
+
+    seq_printf(m, "Buffers\t\t: %lu kB\n", i.bufferram << (PAGE_SHIFT - 10));
+    seq_printf(m, "Active\t\t: %lu kB\n", (pages[LRU_ACTIVE_ANON] +
+                                           pages[LRU_ACTIVE_FILE]) << (PAGE_SHIFT - 10));
+    seq_printf(m, "Inactive\t: %lu kB\n", (pages[LRU_INACTIVE_ANON] +
+                                           pages[LRU_INACTIVE_FILE]) << (PAGE_SHIFT - 10));
+
+
+    seq_printf(m, "Dirty\t\t: %lu kB\n",
+               global_node_page_state(NR_FILE_DIRTY) << (PAGE_SHIFT - 10));
+    seq_printf(m, "Writeback\t: %lu kB\n",
+               global_node_page_state(NR_WRITEBACK) << (PAGE_SHIFT - 10));
+
+    seq_printf(m, "Shmem\t\t: %lu kB\n", i.sharedram << (PAGE_SHIFT - 10));
+
+    seq_printf(m, "KernelStack\t: %lu kB\n",
+               global_zone_page_state(NR_KERNEL_STACK_KB));
+    seq_printf(m, "PageTables\t: %lu kB\n",
+               global_zone_page_state(NR_PAGETABLE) << (PAGE_SHIFT - 10));
 
 
 
-        /*time information*/
-        seq_printf(m, "\n=============Time================\n");
-        
-	nsec = 0;
-	for_each_possible_cpu(j)
-		nsec += (__force u64) kcpustat_cpu(j).cpustat[CPUTIME_IDLE];
 
-	get_monotonic_boottime(&uptime);
-	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
-	idle.tv_nsec = rem;
-	seq_printf(m, "Uptime\t\t: %lu.%02lu (s)\n"
-                      "Idletime\t: %lu.%02lu (s)\n",
-			(unsigned long) uptime.tv_sec,
-			(uptime.tv_nsec / (NSEC_PER_SEC / 100)),
-			(unsigned long) idle.tv_sec,
-			(idle.tv_nsec / (NSEC_PER_SEC / 100)));
 
-	return 0;
+
+    /*time information*/
+    seq_printf(m, "\n=============Time================\n");
+
+    nsec = 0;
+    for_each_possible_cpu(j)
+    nsec += (__force u64) kcpustat_cpu(j).cpustat[CPUTIME_IDLE];
+
+    get_monotonic_boottime(&uptime);
+    idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
+    idle.tv_nsec = rem;
+    seq_printf(m, "Uptime\t\t: %lu.%02lu (s)\n"
+               "Idletime\t: %lu.%02lu (s)\n",
+               (unsigned long) uptime.tv_sec,
+               (uptime.tv_nsec / (NSEC_PER_SEC / 100)),
+               (unsigned long) idle.tv_sec,
+               (idle.tv_nsec / (NSEC_PER_SEC / 100)));
+
+    return 0;
 }
 
 static void *c_start(struct seq_file *m, loff_t *pos)
 {
-	if ((*pos) < 1)
-		return &cpu_data(*pos);
-	return NULL;
+    if ((*pos) < 1)
+        return &cpu_data(*pos);
+    return NULL;
 }
 
 static void *c_next(struct seq_file *m, void *v, loff_t *pos)
 {
-	(*pos)++;
-	return c_start(m, pos);
+    (*pos)++;
+    return c_start(m, pos);
 }
 
 static void c_stop(struct seq_file *m, void *v)
 {
 }
 
-const struct seq_operations info_op = {
-	.start	= c_start,
-	.next	= c_next,
-	.stop	= c_stop,
-	.show	= show_my_info,
+const struct seq_operations info_op =
+{
+    .start	= c_start,
+    .next	= c_next,
+    .stop	= c_stop,
+    .show	= show_my_info,
 };
 
 
-static int my_open(struct inode *inode, struct file *file) 
+static int my_open(struct inode *inode, struct file *file)
 
-{ 
+{
 
-    return seq_open(file, &info_op); 
+    return seq_open(file, &info_op);
 
-}; 
+};
 
- 
 
-static const struct file_operations my_file_ops = { 
 
-    .open = my_open, 
+static const struct file_operations my_file_ops =
+{
 
-    .read = seq_read, 
+    .open = my_open,
 
-}; 
+    .read = seq_read,
 
-static int __init procfs_init(void) 
+};
 
-{ 
+static int __init procfs_init(void)
 
-    struct proc_dir_entry *entry; 
+{
 
-    entry = proc_create(PROC_NAME, 0, NULL, &my_file_ops); 
+    struct proc_dir_entry *entry;
 
-    return 0; 
+    entry = proc_create(PROC_NAME, 0, NULL, &my_file_ops);
 
-} 
+    return 0;
 
- 
+}
 
-static void __exit procfs_exit(void) 
 
-{ 
 
-    remove_proc_entry(PROC_NAME, NULL); 
+static void __exit procfs_exit(void)
 
-    pr_debug("/proc/%s removed\n", PROC_NAME); 
+{
 
-} 
+    remove_proc_entry(PROC_NAME, NULL);
 
- 
+    pr_debug("/proc/%s removed\n", PROC_NAME);
 
-module_init(procfs_init); 
+}
 
-module_exit(procfs_exit); 
 
- 
+
+module_init(procfs_init);
+
+module_exit(procfs_exit);
+
+
 
 MODULE_LICENSE("GPL");
