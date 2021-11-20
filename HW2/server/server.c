e1b004c0e45b5ff3a6d04c1bd302b82f241fb7a2
a43414786 <a43414786@gmail.com>
2021-11-20 11:49:26 +0800

    'k'

--- HW2/server/server.c
+++ HW2/server/server.c
@@ -83,10 +83,11 @@ void* service(void*args){
     bzero(&smsg,sizeof(smsg));
     recv(*forClientSockfd,&rmsg,sizeof(rmsg),0);
     if(strcmp(rmsg.cmd,"SET") == 0){
+        set(smsg.key,smsg.value);
         strcpy(smsg.value,"SET");
     }
     else if(strcmp(rmsg.cmd,"GET") == 0){
-        strcpy(smsg.value,"a");
+        strcpy(smsg.value,get(smsg.key));
     }
     else if(strcmp(rmsg.cmd,"DELETE") == 0){
         strcpy(smsg.value,"DELETE");
