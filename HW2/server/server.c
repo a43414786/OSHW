e1e80bcd63421363c2ff9d88c92ab4d9e97f9471
a43414786 <a43414786@gmail.com>
2021-11-20 12:03:08 +0800

    'k'

--- HW2/server/server.c
+++ HW2/server/server.c
@@ -26,7 +26,7 @@ struct node{
     Node* next;
 };
 
-Node* database[26] = {};
+Node* database[26] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
 
 int tblidx(char* key){
     char head = key[0];
