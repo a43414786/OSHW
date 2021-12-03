32286157ae8f0d8fc4e9aab89f631a71f27234f4
a43414786 <a43414786@gmail.com>
2021-12-03 23:40:22 +0800

    k

--- hw3/simulator.c
+++ hw3/simulator.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <signal.h>
 #include "t.h"
+
+Thread*H_queue = NULL;
+Thread*M_queue = NULL;
+Thread*L_queue = NULL;
+
+
 void handler(){
     printf("a");
 }
@@ -13,7 +19,13 @@ int main(int argc,char** argv)
     
     Thread *root = getthreads();
     while(root){
-        OS2021_ThreadCreate(root->name,root->function,root->priority,root->cancelmode);
+        if(root->priority == 0){
+            addnode_thread(&L_queue,root);
+        }else if(root->priority == 1){
+            addnode_thread(&M_queue,root);
+        }else if(root->priority == 2){
+            addnode_thread(&H_queue,root);
+        }
         root = root->next;
     }
     while(1){
