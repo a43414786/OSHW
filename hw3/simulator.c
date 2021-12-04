f548063fe6b6c8a96317f6293fa9e7ada0083aa3
a43414786 <a43414786@gmail.com>
2021-12-04 16:52:26 +0800

    k

--- hw3/simulator.c
+++ hw3/simulator.c
@@ -1,6 +1,12 @@
 #include "os2021_thread_api.h"
 
 int counter = 0;
+Thread*H_queuef = NULL;
+Thread*H_queuer = NULL;
+Thread*M_queuef = NULL;
+Thread*M_queuer = NULL;
+Thread*L_queuef = NULL;
+Thread*L_queuer = NULL;
 
 void alarm_handler(){
     printf("%d\n",counter++);
