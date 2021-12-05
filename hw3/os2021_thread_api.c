5502140304acec07b3ac6c7a28ee5c73a6461058
a43414786 <a43414786@gmail.com>
2021-12-05 22:05:34 +0800

    k

--- hw3/os2021_thread_api.c
+++ hw3/os2021_thread_api.c
@@ -289,6 +289,7 @@ void Dispatcher()
         root = root->next;
         free(temp);
     }
+    while(1);
     //while(1){
     //    temp = dequeue(&H_queuef,&H_queuer);
     //    swapcontext(&dispatch_context,&(temp->ctx));
