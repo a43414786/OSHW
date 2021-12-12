fdde262c083d352207896d86983a18f34ddf60a3
a43414786 <a43414786@gmail.com>
2021-12-12 15:14:00 +0800

    k

--- hw3/os2021_thread_api.c
+++ hw3/os2021_thread_api.c
@@ -523,7 +523,6 @@ void OS2021_TestCancel()
 {
     Thread *temp = running;
     if(temp->cancelsig){
-        printf("del %s\n",temp->name);
         running = NULL;
         memset(&(temp->state),0,sizeof(temp->state));
         strcpy(temp->state,"TERMINATED");
