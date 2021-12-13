1da4a66bf7e58b3d757cb0f88f99bccedb7b732b
a43414786 <a43414786@gmail.com>
2021-12-13 22:51:54 +0800

    'k'

--- hw3/test.c
+++ hw3/test.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-int main(){
+#include <ucontext.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define STACK_SIZE 8192
+
+void CreateContext(ucontext_t *context, ucontext_t *next_context, void *func)
+{
+    getcontext(context);
+    context->uc_stack.ss_sp = malloc(STACK_SIZE);
+    context->uc_stack.ss_size = STACK_SIZE;
+    context->uc_stack.ss_flags = 0;
+    context->uc_link = next_context;
+    makecontext(context,(void (*)(void))func,0);
+}
+
+void Function2(void)
+{
     int the_num;
 
     int min = 65400;
@@ -18,6 +37,16 @@ int main(){
             min = 0;
             max = 0;
         }
+        puts("Function2");
     }
+}
+
+int main(){
+    ucontext_t test[3];
+
+    CreateContext(&test[0],NULL,&Function2);
+    CreateContext(&test[1],NULL,&Function2);
+    CreateContext(&test[2],NULL,&Function2);
+
     return 0;
 }
