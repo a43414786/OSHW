5ffcad9bb13bcac1c63257db8e10ebdce89cae23
a43414786 <a43414786@gmail.com>
2021-12-13 22:59:43 +0800

    'k'

--- hw3/test.c
+++ hw3/test.c
@@ -40,7 +40,7 @@ void Function2(void)
             min = 0;
             max = 0;
         }
-        printf("%d Function2\n",counter);
+        printf("%d Function2\n",counter%3);
     }
 }
 ucontext_t test[3];
