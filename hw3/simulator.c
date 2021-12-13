0a1fb6bcd9e1447143d9991dd36a24e20bb676e1
a43414786 <a43414786@gmail.com>
2021-12-13 23:04:27 +0800

    'k'

--- hw3/simulator.c
+++ hw3/simulator.c
@@ -12,7 +12,7 @@ int main(int argc,char** argv)
     signal(SIGTSTP,zhandler);
     signal(SIGALRM,handler);
     OS2021_ThreadCreate("random_1","Function2","M",1);
-    while;
+    while(1);
     //StartSchedulingSimulation();
 
     return 0;
