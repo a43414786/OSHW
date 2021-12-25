c89d9acdde3895273538c971777a2011f66c09a6
a43414786 <a43414786@gmail.com>
2021-12-25 22:05:56 +0800

    'k'

--- hw4/MemManager.c
+++ hw4/MemManager.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE*trace = fopen("trace.txt", "r");
-char word;
-while((word = getc(trace)) != EOF){
-    printf("%c",word);
+int main(){
+    FILE*trace = fopen("trace.txt", "r");
+    char word = 0;
+    while((word = getc(trace)) != EOF){
+        printf("%c",word);
+    }
+    return 0;
 }
